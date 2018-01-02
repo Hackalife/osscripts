dbebfdf91c40e41f5384c34be1c3b6a34f727025
Hackalife <Lukas@gerrlich.org>
2018-01-02 14:50:41 +0100

    Now times are the first parameter
    ./timer.sh {time in min}

--- timer.sh
+++ timer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-MIN=1 && for i in $(seq $(($MIN*60)) -1 1);
+MIN=$1 && for i in $(seq $(($MIN*60)) -1 1);
 do echo -n "$i, ";
 sleep 1;
 done;
-osascript -e 'set volume output volume 0';
+#osascript -e 'set volume output volume 0';
 osascript -e 'tell application "iTunes" to pause';
